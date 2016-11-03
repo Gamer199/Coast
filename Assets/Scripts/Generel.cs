6c2208113460b657b861cc9bcfa8e6b9ad50807a
Gamer199 <tobias.goesslbauer@gmail.com>
2016-11-03 01:29:41 +0100

    All Files

---
+++ Assets/Scripts/Generel.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Generel : MonoBehaviour
+{
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour
+{
+    private float scale;
+
+    void Start()
+    {
+        scale = 0.25F;
+        transform.localScale = new Vector3(scale, scale, scale);
+    }
+
+    void Update()
+    {
+        if(scale <= 1)
+        {
+            scale += 0.025F;
+            transform.localScale = new Vector3(scale, scale, scale);
+        }
+        //transform.Rotate(new Vector3(0, 0, 45) * Time.deltaTime);
+    }
+}
